6821db52c9d67d913982509f26cb463d07fd61ce
Harry <harry@harrybird.nz>
2026-02-25 13:58:47 +1100

    new version

--- book_data/__manifest__.py
+++ book_data/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Book Data',
-    'version': '1.1',
+    'version': '1.2',
     'category': 'Retail',
     'summary': 'Fetch book metadata from external APIs (Hardcover)',
     'description': """
