bb8fcc0edcf8826d473218821a4950945441be01
Harry <harry@harrybird.nz>
2026-02-25 13:17:57 +1100

    added author and publisher to product tiles

--- web_search/models/product_template.py
+++ web_search/models/product_template.py
@@ -9,18 +9,4 @@ class ProductTemplate(models.Model):
         result = super()._search_get_detail(website, order, options)
         result['search_fields'].extend(['x_author', 'x_publisher'])
         result['fetch_fields'].extend(['x_author', 'x_publisher'])
-        result['mapping']['subtitle'] = {'name': 'subtitle_text', 'type': 'text'}
         return result
-
-    def _search_render_results(self, fetch_fields, mapping, icon, limit):
-        results_data = super()._search_render_results(fetch_fields, mapping, icon, limit)
-        for product, data in zip(self, results_data):
-            # Create subtitle with author and publisher
-            subtitle_parts = []
-            if product.x_author:
-                subtitle_parts.append(product.x_author)
-            if product.x_publisher:
-                subtitle_parts.append(product.x_publisher)
-            if subtitle_parts:
-                data['subtitle_text'] = ' • '.join(subtitle_parts)
-        return results_data
