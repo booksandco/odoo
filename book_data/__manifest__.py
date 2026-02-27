0c149bc48a50f00045793792c3aa4b962aa359ad
Harry <harry@harrybird.nz>
2026-02-27 12:50:44 +1100

    removed publisher and pub date if not book

--- book_data/__manifest__.py
+++ book_data/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Book Data',
-    'version': '1.9.1',
+    'version': '1.9.2',
     'category': 'Retail',
     'summary': 'Fetch book metadata from external APIs (Hardcover, Titlepage)',
     'description': """
