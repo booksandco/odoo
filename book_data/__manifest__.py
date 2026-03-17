edfc07a48c45b2bd45e102394809c7dfe8e8d179
Harry <harry@harrybird.nz>
2026-03-17 14:27:29 +1100

    added cron job to update data quality

--- book_data/__manifest__.py
+++ book_data/__manifest__.py
@@ -13,6 +13,7 @@ Populates description, author, image, publication date, and AU/NZ pricing on pro
     'data': [
         'security/ir.model.access.csv',
         'data/ir_model_fields.xml',
+        'data/ir_cron.xml',
         'views/product_template_views.xml',
         'views/res_config_settings_views.xml',
     ],
