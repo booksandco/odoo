0711d4053ce1fb3786702f9c6add9a7b3a131323
Harry <harry@harrybird.nz>
2026-03-17 15:24:06 +1100

    change to fraction representation

--- book_data/__manifest__.py
+++ book_data/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Book Data',
-    'version': '1.10.7',
+    'version': '1.10.8',
     'category': 'Retail',
     'summary': 'Fetch book metadata from external APIs (Hardcover, Titlepage)',
     'description': """
