fa970a4b74ca5292ea87165f5da30d39b0e9f4db
Harry <harry@harrybird.nz>
2026-03-18 13:53:25 +1100

    sql fix

--- bookstore/models/__init__.py
+++ bookstore/models/__init__.py
@@ -1,6 +1,6 @@
 from . import product_product
 from . import product_template
 from . import sale_order
-from . import vendor_return_line
 from . import vendor_return_policy
+from . import vendor_return_line
 from . import vendor_return_wizard
