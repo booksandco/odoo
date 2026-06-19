fdf8957af129c73b9af05390fbbdee2407afde0c
Harry <harry@harrybird.nz>
2026-06-19 14:05:52 +1000

    Fix author/publisher links not evaluating t-attf-href
    - Use t-set to compute slug and t-att-href for author/publisher links
    - Fixes 404 from literal {{ }} in product page and product card
    - Bump book_data 19.0.1.2.3 and web_search 19.0.1.0.2

--- book_data/__manifest__.py
+++ book_data/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Book Data',
-    'version': '19.0.1.2.2',
+    'version': '19.0.1.2.3',
     'category': 'Retail',
     'summary': 'Fetch book metadata and reviews from external APIs (Hardcover, Titlepage)',
     'description': """
